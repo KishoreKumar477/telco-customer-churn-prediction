8d59d1c035ab1d1badfe1b5c1bc9beb145177d64
kishorekumarr <kishorekumar040707@gmail.com>
2026-03-09 14:01:57 +0530

    Update config path in load_config function

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,7 +1,7 @@
 # config_loader.py
 import yaml
 
-def load_config(config_path=r"/Users/kishorekumar/Desktop/Telco Customer Churn/config.yaml"):
+def load_config(config_path=r"Telco Customer Churn/config.yaml"):
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
     return config
